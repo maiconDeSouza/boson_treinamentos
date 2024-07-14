2393dabba59992cfc982b8297e22982779ab0681
Maicon Souza <maicon_souza09@hotmail.com>
2024-07-14 20:02:31 -0300

    14-07-24 20:02

---
+++ variaveis.py
@@ -0,0 +1,15 @@
+nome = "Maicon"
+numero = 23
+_nome = "Maicon"
+
+n_1 = n_2 = n_3 = n_4 = 0.0
+m_1, m_2 = 10, 8
+m_3 = m_1, m_2
+
+print(nome, numero, _nome)
+print(n_1, n_4)
+print(m_1, m_2)
+
+print(type(m_3))
+print(isinstance(m_3, tuple))
+print(isinstance(nome, int))
